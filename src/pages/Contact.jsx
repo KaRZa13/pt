cebe31f2632f02e346192fd353bb14af2700e20e
KaRZa13 <rafael.francisco.muro@gmail.com>
2025-03-11 20:49:25 +0100

    2025/03/11 - Almost done

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,20 +1,41 @@
+import { useEffect, useRef } from 'react'
 import DecryptedText from '../components/DecryptedText'
+import ScrollTitle from '../components/ScrollTitle'
+
 
 const Contact = () => {
 
+  const contactDiv = useRef(null)
+
+  useEffect(() => {
+    
+  })
+
   return (
-    <div className="h-screen w-screen flex flex-col justify-center items-center">
-      <h1>Contact</h1>
-      <p>Send us a message!</p>
-      <DecryptedText
-        text="Bonjour je m'appelle Rafael MURO"
-        speed={50}
+    <section className="h-[135vh] w-screen flex flex-col pt-[40vh]">
+      <span className='flex justify-center items-center'>
+        <h2>
+          <ScrollTitle
+            animationDuration={3}
+            ease='back.inOut(2)'
+            scrollStart='center bottom+=50%'
+            scrollEnd='bottom bottom-=30%'
+            stagger={0.1}
+            textClassName='!text-6xl uppercase'
+          >
+            Contactez moi
+          </ScrollTitle>
+        </h2>
+      </span>
+     <DecryptedText
+        text="Bonjour je m'appelle Rafael MURO" 
+        speed={50} 
         characters="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789=+-_:;,?.!@#$%^&*()[]{}|"
         animateOn="view"
         revealDirection="start"
         sequential={true}
-      />
-    </div>
+     />
+    </section>
   )
 }
 
